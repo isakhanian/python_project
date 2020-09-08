f093eebf5dbdcfc136578cc4d8efe1146681f5a0
karine_isakhanian <karine.isakhanian@gmail.com>
2020-09-08 20:39:46 +0300

    add solution

--- solutions/kyu6.py
+++ solutions/kyu6.py
@@ -8,9 +8,18 @@ def tower_builder(n_floors):
     floor += 1
   return result
 
-  def find_in_array(seq, predicate): 
+def find_in_array(seq, predicate): 
   for i in range(len(seq)):
     elem = seq[i]
     if predicate(elem, i):
       return i
   return -1
+
+def sort_string(s, ordering):
+    def key_getter(char):
+      try:
+        return ordering.index(char)
+      except:
+        return len(ordering) + ord(char)
+
+    return ''.join(sorted(s, key=key_getter))
